b5261b90e572c2c1569809bae23e9c78aea4a332
Luca Somaschini <somas99@gmail.com>
2022-07-07 15:38:06 +0200

    Mail - fixed for loop, added comments

--- mail/js/main.js
+++ mail/js/main.js
@@ -8,6 +8,8 @@ const mailList = ['user1@google.com', 'user2@apple.com', 'user3@microsoft.com', 
 for (i=0; i<mailList.length; i++) {
     if (userMail == mailList[i]) {
         alert('Access granted! Your profile page will load shortly.')
-        i = mailList.length;
+        i = mailList.length; // to stop the for loop.
+    } else if (i == (mailList.length-1)) { // only after we have cycled through all the for loop, it is the moment to show the error message.
+        alert('Access not allowed! Please, type a valid email address.');
     }
 }
