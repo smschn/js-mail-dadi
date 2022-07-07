a0adc43324bbb09ca428c55757151b86d194b9b6
Luca Somaschini <somas99@gmail.com>
2022-07-07 14:37:09 +0200

    Dadi - completed js

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,1 +1,16 @@
-console.log('test')
+let humanNumber;
+let computerNumber;
+
+humanNumber = Math.floor(Math.random() * 6 + 1);
+console.log('The number drawn for the human player is ' + humanNumber);
+
+computerNumber = Math.floor(Math.random() * 6 + 1);
+console.log('The number drawn for the human player is ' + computerNumber);
+
+if (humanNumber > computerNumber) {
+    console.log('Human is the winner!');
+} else if (humanNumber < computerNumber) {
+    console.log('Computer is the winner! ...Skynet is not far away');
+} else if (humanNumber == computerNumber) {
+    console.log('It is a draw! Reload the page to test your luck again!');
+}
