88dd52a86d5b7e7acdb9e55961b47ca683ce2ed0
Luca Somaschini <somas99@gmail.com>
2022-07-07 14:42:47 +0200

    Dadi - added comments in js

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,12 +1,16 @@
+// define variables (without value which is given next).
 let humanNumber;
 let computerNumber;
 
+// generate a random number for the human player and assign it to its variable, then write in console.
 humanNumber = Math.floor(Math.random() * 6 + 1);
 console.log('The number drawn for the human player is ' + humanNumber);
 
+// generate a random number for the omputer and assign it to its variable, then write in console.
 computerNumber = Math.floor(Math.random() * 6 + 1);
 console.log('The number drawn for the human player is ' + computerNumber);
 
+// if statement to check who is the winner, then print in console a message. 
 if (humanNumber > computerNumber) {
     console.log('Human is the winner!');
 } else if (humanNumber < computerNumber) {
