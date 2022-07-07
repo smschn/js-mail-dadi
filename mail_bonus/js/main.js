547f4428e7d2b0b5eac3a4940b6ec097b6779808
Luca Somaschini <somas99@gmail.com>
2022-07-07 16:02:01 +0200

    Mail_bonus - added event listener

--- mail_bonus/js/main.js
+++ mail_bonus/js/main.js
@@ -1,15 +1,25 @@
-// user input.
-const userMail = prompt('Please, enter your email address.');
+// user input taken from input.
+const userMail = document.getElementById('mail');
+
+
 
 // declare variable (array).
 const mailList = ['user1@google.com', 'user2@apple.com', 'user3@microsoft.com', 'user4@boolean.com']
 
-// for loop to cycle through the array to check if the mail address is inside the array.
-for (i=0; i<mailList.length; i++) {
-    if (userMail == mailList[i]) {
-        alert('Access granted! Your profile page will load shortly.')
-        i = mailList.length; // to stop the for loop.
-    } else if (i == (mailList.length-1)) { // only after we have cycled through all the for loop, it is the moment to show the error message.
-        alert('Access not allowed! Please, type a valid email address.');
+// click
+const buttonClick = document.getElementById('sentMail');
+buttonClick.addEventListener('click',
+function() {
+
+
+
+    // for loop to cycle through the array to check if the mail address is inside the array.
+    for (i=0; i<mailList.length; i++) {
+        if (userMail.value == mailList[i]) {
+            alert('Access granted! Your profile page will load shortly.')
+            i = mailList.length; // to stop the for loop.
+        } else if (i == (mailList.length-1)) { // only after we have cycled through all the for loop, it is the moment to show the error message.
+            alert('Access not allowed! Please, type a valid email address.');
+        }
     }
-}
+})
