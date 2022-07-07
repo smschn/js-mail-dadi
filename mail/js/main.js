17ddb043f7b03c5484eeddc2f0883d8410054111
Luca Somaschini <somas99@gmail.com>
2022-07-07 15:10:47 +0200

    Mail - added exits in for loop

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,10 +1,14 @@
 const userMail = prompt('Please, enter your email address.');
 
-const mailList = ['user1@google.com', 'user2@apple.com', 'user3@microsoft.com', 'user4@boolean.com',]
+const mailList = ['user1@google.com', 'user2@apple.com', 'user3@microsoft.com', 'user4@boolean.com']
 
 for (i=0; i<mailList.length; i++) {
     if (userMail == mailList[i]) {
-        alert('Access granted! Your profile page will load shortly')
+        alert('Access granted! Your profile page will load shortly.')
+        i = mailList.length;
+    } else {
+        alert('Access not allawed! Please, enter a correct email address.')
+        i = mailList.length;
     }
 }
 
