0cbe338be2c2b5dc5317fbb7bf4846f28d565543
Luca Somaschini <somas99@gmail.com>
2022-07-07 19:11:17 +0200

    Mail_bonus - Added dynamic css to output (bonus)

--- mail_bonus/js/main.js
+++ mail_bonus/js/main.js
@@ -24,9 +24,11 @@ function() {
             // alert('Access granted! Your profile page will load shortly.')
             i = mailList.length; // to stop the for loop.
             outputDom.innerHTML = 'Access granted! Your profile page will load shortly.'; // to show the result on html page.
+            outputDom.className = 'container outputClass_1'; // to remove the classes outputDom already has && to add these new classes.
         } else if (i == (mailList.length-1)) { // only after we have cycled through all the for loop, it is the moment to show the error message.
             // alert('Access not allowed! Please, type a valid email address.');
             outputDom.innerHTML = 'Access not allowed! Please, type a valid email address.'; // to show the result on html page.
+            outputDom.className = 'container outputClass_2'; // to remove the classes outputDom already has && to add these new classes.
         }
     }
 })
