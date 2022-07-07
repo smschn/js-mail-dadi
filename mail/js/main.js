951eb6114e37be3f8446ceb639ec6beb961d5e60
Luca Somaschini <somas99@gmail.com>
2022-07-07 15:28:27 +0200

    Mail - removed code not working, added comments

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,17 +1,13 @@
+// user input.
 const userMail = prompt('Please, enter your email address.');
 
+// declare variable (array).
 const mailList = ['user1@google.com', 'user2@apple.com', 'user3@microsoft.com', 'user4@boolean.com']
 
+// for loop to cycle through the array to check if the mail address is inside the array.
 for (i=0; i<mailList.length; i++) {
     if (userMail == mailList[i]) {
         alert('Access granted! Your profile page will load shortly.')
         i = mailList.length;
-    } else {
-        alert('Access not allawed! Please, enter a correct email address.')
-        i = mailList.length;
     }
 }
-
-// debug
-console.log(userMail);
-console.log(mailList);
